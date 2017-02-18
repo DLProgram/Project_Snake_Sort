fdef16848de3e2002bffadcf5327224ad80a5525
Daniel Luan <DanielLuan425@gmail.com>
2017-02-17 22:40:27 -0500

    Comment database file

--- database.py
+++ database.py
@@ -4,6 +4,15 @@ db = peewee.MySQLDatabase('snake_sort', user='snake', passwd='password')
 
 
 class match(peewee.Model):
+    """Table storing all the match data.
+
+    Args:
+        match_id(int): Id of the match.
+        blue1(char): Blue Team Name 1.
+        blue2(char): Blue Team Name 2.
+        red1(char): Red Team Name 1.
+        red2(char): Red Team Name 2.
+    """
     match_id = peewee.IntegerField()
     blue1 = peewee.CharField()
     blue2 = peewee.CharField()
